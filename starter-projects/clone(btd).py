b32e9f4a39b47365d6bc64f6f6c5b813449af231
andoh <86951347+1andxh@users.noreply.github.com>
2024-08-21 23:00:27 +0000

    Delete starter-projects directory

--- starter-projects/clone(btd).py
+++
@@ -1,6 +0,0 @@
-class NumberConversion:
-    def to_decimal(self,binary):
-        self.binary = list(str(binary))
-        self.dummy = list()
-        self.power = 0
-        
--- starter-projects/hr.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-
-str_n = f'{n}'
-for n in range(n + 1):
-    print(str_n)
